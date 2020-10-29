b4db86f72cffc99fcddc89e901ea131a4b2b1a93
GoDoyeon <97godo@naver.com>
2020-10-29 13:59:05 +0900

    gs-spring-boot2

---
+++ src/main/java/springguides/gsspringgboot/GsSpringBootApplication.java
@@ -0,0 +1,13 @@
+package springguides.gsspringgboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GsSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GsSpringBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/springguides/gsspringgboot/GsSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package springguides.gsspringgboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GsSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
